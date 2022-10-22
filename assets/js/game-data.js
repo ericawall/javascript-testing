c9adefd000ec94c481591e9b5dc3b6d66636f265
ericawall <erica.wall@hotmail.com>
2022-10-22 19:42:12 +0000

    make game box open after start game is pressed

--- assets/js/game-data.js
+++ assets/js/game-data.js
@@ -2,337 +2,211 @@
 * The game-data.js file contains the images to display for different game level selected by the user,
 * phrases to be guessed and functions to select random phrase from various categories.
 */
-let gameSetup = (function () {
-    let easyLevelImages = [
-        'assets/images/sun-image-easy0.png',
-        'assets/images/sun-image-easy1.png',
-        'assets/images/sun-image-easy2.png',
-        'assets/images/sun-image-easy3.png',
-        'assets/images/sun-image-easy4.png',
-        'assets/images/sun-image-easy5.png',
-        'assets/images/sun-image-easy6.png',
-        'assets/images/sun-image-easy7.png'
-    ];
+var COLS = 10, ROWS = 20;
+var board = [];
+var lose;
+var interval;
+var intervalRender;
+var current; // current moving shape
+var currentX, currentY; // position of current shape
+var freezed; // is current shape settled on the board?
+var shapes = [
+    [ 1, 1, 1, 1 ],
+    [ 1, 1, 1, 0,
+      1 ],
+    [ 1, 1, 1, 0,
+      0, 0, 1 ],
+    [ 1, 1, 0, 0,
+      1, 1 ],
+    [ 1, 1, 0, 0,
+      0, 1, 1 ],
+    [ 0, 1, 1, 0,
+      1, 1 ],
+    [ 0, 1, 0, 0,
+      1, 1, 1 ]
+];
+var colors = [
+    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+];
 
-    let mediumLevelImages = [
-        'assets/images/sun-image-medium0.png',
-        'assets/images/sun-image-medium1.png',
-        'assets/images/sun-image-medium2.png',
-        'assets/images/sun-image-medium3.png',
-        'assets/images/sun-image-medium4.png',
-        'assets/images/sun-image-medium5.png',
+// creates a new 4x4 shape in global variable 'current'
+// 4x4 so as to cover the size when the shape is rotated
+function newShape() {
+    var id = Math.floor( Math.random() * shapes.length );
+    var shape = shapes[ id ]; // maintain id for color filling
 
-    ];
+    current = [];
+    for ( var y = 0; y < 4; ++y ) {
+        current[ y ] = [];
+        for ( var x = 0; x < 4; ++x ) {
+            var i = 4 * y + x;
+            if ( typeof shape[ i ] != 'undefined' && shape[ i ] ) {
+                current[ y ][ x ] = id + 1;
+            }
+            else {
+                current[ y ][ x ] = 0;
+            }
+        }
+    }
+    
+    // new shape starts to move
+    freezed = false;
+    // position where the shape will evolve
+    currentX = 5;
+    currentY = 0;
+}
 
-    let hardLevelImages = [
-        'assets/images/sun-image-hard0.png',
-        'assets/images/sun-image-hard1.png',
-        'assets/images/sun-image-hard2.png',
-        'assets/images/sun-image-hard3.png'
-    ];
+// clears the board
+function init() {
+    for ( var y = 0; y < ROWS; ++y ) {
+        board[ y ] = [];
+        for ( var x = 0; x < COLS; ++x ) {
+            board[ y ][ x ] = 0;
+        }
+    }
+}
 
-    let phrases = {
-        people: [
-            'Barack Obama',
-            'Marilyn Monroe',
-            'Elvis Presley',
-            'Freddie Mercury',
-            'Audrey Hepburn',
-            'Michael Jackson',
-            'Steven Spielberg',
-            'Nelson Mandela',
-            'Coco Chanel',
-            'Rafael Nadal',
-            'Serena Williams',
-            'Ellen DeGeneres',
-            'Halle Berry',
-            'Alfred Hitchcock',
-            'Ernest Hemingway',
-            'George Orwell',
-            'Oscar Wilde',
-            'Justin Bieber',
-            'Leonardo da Vinci',
-            'Ludwig van Beethoven'
-        ],
+// keep the element moving down, creating new shapes and clearing lines
+function tick() {
+    if ( valid( 0, 1 ) ) {
+        ++currentY;
+    }
+    // if the element settled
+    else {
+        freeze();
+        valid(0, 1);
+        clearLines();
+        if (lose) {
+            clearAllIntervals();
+            return false;
+        }
+        newShape();
+    }
+}
 
-        book: [
-            'One Hundred Years of Solitude',
-            'Pride and Prejudice',
-            'To Kill a Mockingbird',
-            'Crime and Punishment',
-            'The Master and Margarita',
-            'The Lord of the Rings',
-            'Charlie and the Chocolate Factory',
-            'The Secret Garden',
-            'Anna Karenina',
-            'Little Women',
-            'Don Quixote',
-            'A Christmas Carol',
-            'Anne of Green Gables',
-            'Oliver Twist',
-            'Ulysses',
-            'War and Peace',
-            'The Divine Comedy',
-            'Nineteen Eighty Four',
-            'Les Miserables',
-            'Gone With the Wind'
-        ],
+// stop shape at its position and fix it to board
+function freeze() {
+    for ( var y = 0; y < 4; ++y ) {
+        for ( var x = 0; x < 4; ++x ) {
+            if ( current[ y ][ x ] ) {
+                board[ y + currentY ][ x + currentX ] = current[ y ][ x ];
+            }
+        }
+    }
+    freezed = true;
+}
 
-        film: [
-            'The Godfather',
-            'No Country For Old Men',
-            'The Shawshank Redemption',
-            'The Last Emperor',
-            'Avatar',
-            'The Devil Wears Prada',
-            'Silence of the Lambs',
-            'Slumdog Millionaire',
-            'Eternal Sunshine of the Spotless Mind',
-            'Fight Club',
-            'Saving Private Ryan',
-            'American History X',
-            'Pulp Fiction',
-            'American Pie',
-            'Mean Girls',
-            'Back to the Future',
-            'Catch Me If You Can',
-            'Being John Malkovich',
-            'The Hangover',
-            'Requiem for a Dream'
-        ],
-
-        building: [
-            'La Sagrada Familia',
-            'The Colosseum',
-            'Empire State Building',
-            'Leaning Tower of Pisa',
-            'Eiffel Tower',
-            'Taj Mahal',
-            'Haga Sophia',
-            'Space Needle',
-            'Big Ben',
-            'Parthenon',
-            'The White House',
-            'Louvre Museum',
-            'Great Pyramid of Giza',
-            'Burj Al Arab',
-            'Sistine Chapel',
-            'Palace of Versailles',
-            'Westminster Abbey',
-            'Sydney Opera House',
-            'Notre Dame',
-            'Chrysler Building'
-        ],
-
-        proverb: [
-            'A bird in the hand is worth two in the bush',
-            'A barking dog never bites',
-            'A good man is hard to find',
-            'All good things come to an end',
-            'Actions speak louder than words',
-            'An apple a day keeps the doctor away',
-            'Beauty is in the eye of the beholder',
-            'A bad workman always blames his tools',
-            'A fool and his money are soon parted',
-            'Practice makes perfect',
-            'Slow and steady wins the race',
-            'Early bird catches the worm',
-            'Good things come to those who wait',
-            'Keep your mouth shut and your eyes open',
-            'Laughter is the best medicine',
-            'Lightning never strikes twice in the same place',
-            'Never put off until tomorrow what you can do today',
-            'The grass is greener on the other side of the fence',
-            'The road to hell is paved with good intentions',
-            'Too many cooks spoil the broth'
-        ],
-
-        brand: [
-            'Nike',
-            'Adidas',
-            'Tommy Hilfiger',
-            'Giorgio Armani',
-            'Samsung',
-            'Microsoft',
-            'Netflix',
-            'Tesla Motors',
-            'Unilevel',
-            'Versace',
-            'Mastercard',
-            'New Balance',
-            'KFC',
-            'Lego',
-            'Honda',
-            'Amazon',
-            'Starbucks',
-            'Porshe',
-            'Visa',
-            'Hilton'
-        ],
-
-        instrument: [
-            'Saxophone',
-            'Ukulele',
-            'Electric Guitar',
-            'Trumpet',
-            'Clarinet',
-            'Accordion',
-            'Xylophone',
-            'Acoustic Guitar',
-            'Electronic Keyboard',
-            'Trombone',
-            'Bass Guitar',
-            'Mandolin',
-            'French Horn',
-            'Bassoon',
-            'Viola',
-            'Tambourine',
-            'Bagpipes',
-            'Marimba',
-            'Banjo',
-            'Double Bass'
-        ],
-
-        country: [
-            'Antigua and Barbuda',
-            'Azerbaijan',
-            'Barbados',
-            'Bhutan',
-            'Bosnia and Herzegovina',
-            'Cameroon',
-            'Costa Rica',
-            'Cyprus',
-            'Democratic Republic of the Congo',
-            'Dominican Republic',
-            'Ecuador',
-            'Finland',
-            'Georgia',
-            'Honduras',
-            'Ireland',
-            'Jordan',
-            'Kazakhstan',
-            'Liechtenstein',
-            'Mauritania',
-            'Namibia',
-            'Oman',
-            'Poland',
-            'Rwanda',
-            'San Marino',
-            'Spain',
-            'Trinidad and Tobago',
-            'Uruguay',
-            'Veneuela',
-            'Yemen',
-            'Zimbabwe'
-        ],
-
-        sport: [
-            'Volleyball',
-            'Tennis',
-            'Baseball',
-            'Golf',
-            'Basketball',
-            'Swimming',
-            'Table tennis',
-            'Snowboarding',
-            'Skiing',
-            'Football',
-            'Boxing',
-            'Badminton',
-            'Bungee jumping',
-            'Sky diving',
-            'Judo',
-            'Scuba diving',
-            'Weight lifting',
-            'Fencing',
-            'Gymnastics',
-            'Cycling',
-            'Karate',
-            'Surfing',
-            'Cricket',
-            'Rugby',
-            'Bowling'
-        ],
-
-        animal: [
-            'Dog',
-            'Cat',
-            'Horse',
-            'Sheep',
-            'Goat',
-            'Donkey',
-            'Monkey',
-            'Cow',
-            'Pig',
-            'Tiger',
-            'Elephant',
-            'Lion',
-            'Zebra',
-            'Camel',
-            'Rabbit',
-            'Alpaca',
-            'Hoppopotamus',
-            'Octupus',
-            'Penguin',
-            'Mouse',
-            'Chicken',
-            'Hamster',
-            'Crocodile',
-            'Bear',
-            'Dolphin'
-        ]
-    };
-
-    /**
-     * Displays the relevant image depending on the level of game selected by the user
-     * @param {string} gameLevel 
-     * @returns images array
-     */
-    function showImages(gameLevel) {
-        if (gameLevel === "easy") {
-            return easyLevelImages;
-        } else if (gameLevel === "medium") {
-            return mediumLevelImages;
-        } else if (gameLevel === "hard") {
-            return hardLevelImages;
-        } else {
-            throw new Error("Sorry, images do not exist.");
+// returns rotates the rotated shape 'current' perpendicularly anticlockwise
+function rotate( current ) {
+    var newCurrent = [];
+    for ( var y = 0; y < 4; ++y ) {
+        newCurrent[ y ] = [];
+        for ( var x = 0; x < 4; ++x ) {
+            newCurrent[ y ][ x ] = current[ 3 - x ][ y ];
         }
     }
 
-    /**
-     * Select a category and a phrase to guess and return them in an object
-     * @returns {category, phrase}
-     */
-    function getGuessingParameters() {
-        let category = getCategory();
-        let phrase = getPhrase(category);
-        return { category, phrase };
-    }
+    return newCurrent;
+}
 
-    /**
-     * Randomly select a category for the game
-     * @returns {string} selected category
-     */
-    function getCategory() {
-        let categories = Object.keys(phrases);
-        let index = Math.floor(Math.random() * categories.length);
-        return categories[index];
+// check if any lines are filled and clear them
+function clearLines() {
+    for ( var y = ROWS - 1; y >= 0; --y ) {
+        var rowFilled = true;
+        for ( var x = 0; x < COLS; ++x ) {
+            if ( board[ y ][ x ] == 0 ) {
+                rowFilled = false;
+                break;
+            }
+        }
+        if ( rowFilled ) {
+            document.getElementById( 'clearsound' ).play();
+            for ( var yy = y; yy > 0; --yy ) {
+                for ( var x = 0; x < COLS; ++x ) {
+                    board[ yy ][ x ] = board[ yy - 1 ][ x ];
+                }
+            }
+            ++y;
+        }
     }
+}
 
-    /**
-     * Randomly select a guessing phrase for the game
-     * @param {string} category 
-     * @returns {string} phrase
-     */
-    function getPhrase(category) {
-        let categoryPhrases = phrases[category];
-        let index = Math.floor(Math.random() * categoryPhrases.length);
-        return categoryPhrases[index];
+function keyPress( key ) {
+    switch ( key ) {
+        case 'left':
+            if ( valid( -1 ) ) {
+                --currentX;
+            }
+            break;
+        case 'right':
+            if ( valid( 1 ) ) {
+                ++currentX;
+            }
+            break;
+        case 'down':
+            if ( valid( 0, 1 ) ) {
+                ++currentY;
+            }
+            break;
+        case 'rotate':
+            var rotated = rotate( current );
+            if ( valid( 0, 0, rotated ) ) {
+                current = rotated;
+            }
+            break;
+        case 'drop':
+            while( valid(0, 1) ) {
+                ++currentY;
+            }
+            tick();
+            break;
     }
+}
 
-    return {
-        showImages,
-        getGuessingParameters
-    };
-})();
+// checks if the resulting position of current shape will be feasible
+function valid( offsetX, offsetY, newCurrent ) {
+    offsetX = offsetX || 0;
+    offsetY = offsetY || 0;
+    offsetX = currentX + offsetX;
+    offsetY = currentY + offsetY;
+    newCurrent = newCurrent || current;
+
+    for ( var y = 0; y < 4; ++y ) {
+        for ( var x = 0; x < 4; ++x ) {
+            if ( newCurrent[ y ][ x ] ) {
+                if ( typeof board[ y + offsetY ] == 'undefined'
+                  || typeof board[ y + offsetY ][ x + offsetX ] == 'undefined'
+                  || board[ y + offsetY ][ x + offsetX ]
+                  || x + offsetX < 0
+                  || y + offsetY >= ROWS
+                  || x + offsetX >= COLS ) {
+                    if (offsetY == 1 && freezed) {
+                        lose = true; // lose if the current shape is settled at the top most row
+                        document.getElementById('playbutton').disabled = false;
+                    } 
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function playButtonClicked() {
+    newGame();
+    document.getElementById("playbutton").disabled = true;
+}
+
+function newGame() {
+    clearAllIntervals();
+    intervalRender = setInterval( render, 30 );
+    init();
+    newShape();
+    lose = false;
+    interval = setInterval( tick, 400 );
+}
+
+function clearAllIntervals(){
+    clearInterval( interval );
+    clearInterval( intervalRender );
+}
